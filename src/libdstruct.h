427a25516134a34c9e52245f5899b15bb831dd84
error.d <error.d@gmail.com>
2013-01-18 19:58:53 +0800

    add libdstruct.h

---
+++ src/libdstruct.h
@@ -0,0 +1,9 @@
+#ifndef _LIBDSTRUCT_H_
+#define _LIBDSTRUCT_H_
+
+#include <libdstruct/dsbase.h>
+#include <libdstruct/dsioformat.h>
+#include <libdstruct/dstruct.h>
+#include <libdstruct/dsconfig.h>
+
+#endif /* _LIBDSTRUCT_H_ */
